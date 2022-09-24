26fba4f2a5589c5e80926947267cf1d84111cd4e
Lucas Bovolini <lbovolini94@gmail.com>
2022-09-24 15:00:05 -0300

    modifies python version and artifact name

--- lambda.tf
+++ lambda.tf
@@ -2,10 +2,10 @@ resource "aws_lambda_function" "safebot" {
   function_name = "verify"
   handler       = "main.lambda_handler"
   role          = aws_iam_role.safebot_lambda_role.arn
-  runtime       = "python3.10"
+  runtime       = "python3.9"
 
-  filename         = "api.zip"
-  source_code_hash = filebase64sha256("api.zip")
+  filename         = "safebot.zip"
+  source_code_hash = filebase64sha256("safebot.zip")
 
   timeout     = 20
   memory_size = 128
