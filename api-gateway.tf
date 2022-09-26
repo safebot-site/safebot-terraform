1907ab08afb0fd01964a788a551b3891dcb093bc
Lucas Bovolini <lbovolini94@gmail.com>
2022-09-26 16:36:57 -0300

    changed protocol to REST

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,6 +1,6 @@
 resource "aws_apigatewayv2_api" "lambda" {
   name          = "serverless_lambda_gw"
-  protocol_type = "HTTP"
+  protocol_type = "REST"
 }
 
 resource "aws_apigatewayv2_stage" "lambda" {
