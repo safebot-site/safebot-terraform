804a35e5f2ed474e28834da918995c414defda7d
Lucas Bovolini <lbovolini94@gmail.com>
2022-09-26 16:44:51 -0300

    changed protocol to HTTP and fixed route key

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,12 +1,12 @@
 resource "aws_apigatewayv2_api" "lambda" {
   name          = "serverless_lambda_gw"
-  protocol_type = "REST"
+  protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_stage" "lambda" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  name        = "serverless_lambda_stage"
+  name        = "dev"
   auto_deploy = true
 
   access_log_settings {
@@ -39,7 +39,7 @@ resource "aws_apigatewayv2_integration" "safebot" {
 resource "aws_apigatewayv2_route" "safebot" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  route_key = "ANY /{proxy+}"
+  route_key = "ANY /verify"
   target    = "integrations/${aws_apigatewayv2_integration.safebot.id}"
 }
 
