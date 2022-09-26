35663aae7918fe099087823647138a4c075be753
Lucas Bovolini <lbovolini94@gmail.com>
2022-09-26 15:20:58 -0300

    remove test configuration

--- main.tf
+++ main.tf
@@ -11,12 +11,4 @@ provider "aws" {
   region                      = "sa-east-1"
   access_key                  = "test"
   secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    lambda = "http://0.0.0.0:4566"
-    iam    = "http://0.0.0.0:4566"
-  }
 }
